5fd23a48125de1f64bf992ce984d26dc733856cb
iamtonmoy0 <tonmoy2code@gmail.com>
2023-10-25 08:42:37 -0400

    controller and services added

---
+++ controllers/academic/subject.controller.js
@@ -0,0 +1,1 @@
+/home/edith/Desktop/code/Node/school management system/models/Academic/subject.model.js
---
+++ services/academic/academicTerm.services.js
@@ -0,0 +1,1 @@
+/home/edith/Desktop/code/Node/school management system/models/Academic/academicTerm.model.js
