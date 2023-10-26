e9df0c56509a1ae6fbcbf82aa924342f9a22d40c
iamtonmoy0 <tonmoy2code@gmail.com>
2023-10-26 04:05:23 -0400

    controller ,services,routes file added

--- app/app.js
+++ app/app.js
@@ -11,18 +11,20 @@ app.use(express.json());
 app.use(morgan('dev')); // Log requests to the console (Express 4)
 
 // Initialize staff route
-const staffPath = path.join(__dirname, '../routes/staff');
+const routePath = path.join(__dirname, '../routes');
 
 // Use readdirSync to read the files in the staff directory
-readdirSync(staffPath).forEach((fileName) => {
+readdirSync(`${routePath}/staff`).map((fileName) => {
   // Construct the full path to the route file
-  const routeFilePath = path.join(staffPath, fileName);
-
+  const routeFilePath = path.join(`${routePath}/staff`, fileName);
   // Require the route file and use it with '/api/v1' as the base path
   app.use('/api/v1', require(routeFilePath));
 });
-
-// const academicPath = path.join(__dirname,"../routes/academic")
+// Academic route
+// readdirSync(`${routePath}/academic`).map((fileName)=>{
+//   const academicRoutePath = path.join(`${routePath}/academic`,fileName);
+//   app.use("/api/v1",require(academicRoutePath));
+// })
 // Define a default route
 app.get('/', (req, res) => {
   res.send('Server is running!');
