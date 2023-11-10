71b0efa352e17c2f903219209dfab83af19d734b
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-11-11 03:41:29 +0600

    chore: Update student controller and service for better structure
    feat: Implement response handling using response handler
    
    fix: Resolve bugs in student controller and service
    
    - Fix issue with unused variable in studentUpdateProfileService
    - Ensure consistent use of responseStatus in controllers
    - Improve error handling in service functions
    
    docs: Update documentation in student controller and service
    
    feat: Add student routes for registration, login, profile, and updates
    
    - Implement routes for admin registration, student login, profile retrieval, admin updates, etc.
    - Use middleware for authentication and authorization checks

--- routes/v1/students/students.router.js
+++ routes/v1/students/students.router.js
@@ -1,6 +1,42 @@
 const express = require('express');
-const studentRouter = express.Router();
+const studentsRouter = express.Router();
 
-// Define academic routes here
+// Middleware
+const isLoggedIn = require('../../../middlewares/isLoggedIn');
+const isAdmin = require('../../../middlewares/isAdmin');
+const isStudent = require('../../../middlewares/isStudent');
 
-module.exports = studentRouter;
+// Controllers
+const {
+  adminRegisterStudentController,
+  studentLoginController,
+  getStudentProfileController,
+  getAllStudentsByAdminController,
+  getStudentByAdminController,
+  studentUpdateProfileController,
+  adminUpdateStudentController,
+} = require('../../../controllers/students/students.controller');
+
+// Create Student by Admin
+studentsRouter.route('/admin/register').post(isLoggedIn, isAdmin, adminRegisterStudentController);
+
+// Student Login
+studentsRouter.route('/login').post(studentLoginController);
+
+// Get Student Profile
+studentsRouter.route('/profile').get(isLoggedIn, isStudent, getStudentProfileController);
+
+// Get All Students by Admin
+studentsRouter.route('/admin/students').get(isLoggedIn, isAdmin, getAllStudentsByAdminController);
+
+// Get Single Student by Admin
+studentsRouter.route('/:studentId/admin').get(isLoggedIn, isAdmin, getStudentByAdminController);
+
+// Update Student Profile by Student
+studentsRouter.route('/update').patch(isLoggedIn, isStudent, studentUpdateProfileController);
+
+// Admin Update Student Profile
+studentsRouter.route('/:studentId/update/admin').patch(isLoggedIn, isAdmin, adminUpdateStudentController);
+
+module.exports = studentsRouter;
+
